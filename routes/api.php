3324a7707aadfc60d630c07ead2d2ed752414fce
congsu1408 <congsu196@gmail.com>
2024-07-17 23:30:51 +0700

    1st commit

---
+++ routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+
+Route::middleware('api')->group(function () {
+    Route::apiResource('tasks', TaskController::class);
+});
